82fb6d71d9c6c755bd25000e2038aabcf7fa9455
DiNitride <j.ptb.t.ca@gmail.com>
2018-10-05 11:46:03 +0100

    Added Star Queue system

---
+++ stars.py
@@ -0,0 +1,31 @@
+
+class Star:
+    """
+    Represents a Starred message
+    """
+
+    def __init__(self, message_id, author, title, content, img_url):
+        self.message_id = message_id
+        self.author = author
+        self.title = title
+        self.content = content
+        self.img_url = img_url
+
+
+class StarQueue:
+    """
+    A FIFO queue of stars
+    """
+    
+    def __init__(self):
+        self.queue = []
+        
+    @property
+    def queue(self):
+        return self.queue
+
+    def append(self, star):
+        self.queue.append(star)
+
+    def pop(self):
+        return self.queue.pop(0)
